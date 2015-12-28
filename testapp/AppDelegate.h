995aa03524f3bee54c7298bb7a32be21b2070176
Ishant tiwari <ios@techwatts.in>
2015-12-28 22:14:20 +0530

    initial code add

---
+++ testapp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  testapp
+//
+//  Created by Mac Mini on 22/11/15.
+//  Copyright © 2015 dev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
